da89e62f8ffe985f66deeacafc6ca7baa393adf0
Ahmedarshad14 <meahmed1@hotmail.com>
2023-10-09 05:15:15 +0500

    updated navbar

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,12 @@
 import Hero from "./Hero"
 import CustomButton from "./CustomButton"
+import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
 
 
 export {
+    Navbar,
     Hero,
-    CustomButton
+    CustomButton,
+    Footer
 }
