e382003885b5b5ba69fd7154e60fe77cf66bf1e4
Ahmedarshad14 <meahmed1@hotmail.com>
2023-10-11 04:14:04 +0500

    Added headlessui, constants.ts

--- src/components/SearchManufacturer.tsx
+++ src/components/SearchManufacturer.tsx
@@ -1,22 +1,42 @@
-'use client'
-import { Combobox, Transition } from '@headlessui/react'
-import Image from 'next/image'
-import { SearchManufacturerProps } from '../../types'
-const SearchManufacturer = ({manufacturer, setManufacturer} : SearchManufacturerProps ) => {
-  return (
-    <div className='search-manufacturer'>
-      <Combobox>
-        <div className="relative w-full">
-          <Combobox.Button className='absolute top-[14px]'>
-            <Image src="/car-logo.svg" width={20} height={20} alt='Car Logo' className='ml-4' />
+"use client";
+import { useState, Fragment } from "react";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { SearchManufacturerProps } from "../../types";
+import { manufacturers } from '@/constants.ts'
+const SearchManufacturer = ({
+	manufacturer,
+	setManufacturer,
+}: SearchManufacturerProps) => {
+	const [query, setQuery] = useState("");
+	return (
+		<div className="search-manufacturer">
+			<Combobox>
+				<div className="relative w-full">
+					<Combobox.Button className="absolute top-[14px]">
+						<Image
+							src="/car-logo.svg"
+							width={20}
+							height={20}
+							alt="Car Logo"
+							className="ml-4"
+						/>
+					</Combobox.Button>
+					<Combobox.Input
+						className="search-manufacturer__input"
+						placeholder="Honda"
+						displayValue={(manufacturer: string) => manufacturer}
+						onChange={(e) => setQuery(e.target.value)}
+					/>
+          <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0" afterLeave={() => setQuery('')}>
+            <Combobox.Options>
 
-          </Combobox.Button>
-          <Combobox.Input className='search-manufacturer__input'placeholder='Honda'/>
-        </div>
-      </Combobox>
+            </Combobox.Options>
+          </Transition>
+				</div>
+			</Combobox>
+		</div>
+	);
+};
 
-    </div>
-  )
-}
-
-export default SearchManufacturer
+export default SearchManufacturer;
